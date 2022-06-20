7d6f379c253fd87a025f1b91c0c5595ff196c78a
NoahAncolie <noahancolie@gmail.com>
2022-06-20 17:41:12 +0200

    saving index, assets, nav.scss

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import "../../assets/styles/nav.scss"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <p>Page d'accueil</p>
+        </>
+    )
+}
+
+export default Home
