a0b57e1552a6325af5e548298d75bc64cd87c0da
Non0-13 <arnaud.lucciarini@gmail.com>
2022-06-22 17:39:13 +0200

    add redirect for devise links

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,6 +6,7 @@ import Cookies from "js-cookie";
 import { useSetAtom, useAtomValue } from "jotai";
 import { userAtom, JWT } from "store/atoms";
 
+
 const Navbar = () => {
 
     const setToken = useSetAtom(JWT);
@@ -42,7 +43,7 @@ const Navbar = () => {
             secure: true
             })
             setToken("");
-            setUser("");  
+            setUser(""); 
     }
 
     return (
@@ -87,7 +88,7 @@ const Navbar = () => {
                             </svg>
                             &nbsp;&nbsp;Profil
                         </Link></li>
-                        <li><Link to="/disconnect" onClick={() => { ToggleNav(); logout();}}>
+                        <li><Link to="/connect" onClick={() => { ToggleNav(); logout();}}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                                 <path d="M624 448h-80V113.45C544 86.19 522.47 64 496 64H384v64h96v384h144c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM312.24 1.01l-192 49.74C105.99 54.44 96 67.7 96 82.92V448H16c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h336V33.18c0-21.58-19.56-37.41-39.76-32.17zM264 288c-13.25 0-24-14.33-24-32s10.75-32 24-32 24 14.33 24 32-10.75 32-24 32z" />
                             </svg>
