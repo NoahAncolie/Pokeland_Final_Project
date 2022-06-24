84bfd684251573d9efc88d7e536327849e097d91
NoahAncolie <noahancolie@gmail.com>
2022-06-24 17:23:51 +0200

    Ajustements des produits Et Profil

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,18 +12,18 @@ const Home = () => {
         <div className='homepage'>
             <h1 className='home-title'><span className="poke-letter grow">Pokéland</span></h1>
             <div className="websiteInfos row">
-                <div className="col-lg-8"></div>
+                <div className="col-lg-8 col-sm-0"></div>
                 <div className="col-lg-3 webInfosVisible">
                     <p>Sur ce site, vous pourrez...</p>
                     <ul className='webInfo-list'>
                         <li>
-                            Trouver des cartes pokémons rares.
+                            Trouver des cartes pokémons rares.<br/><br/>
                         </li>
                         <li>
-                            Commander ces cartes auprès de collectionneurs.
+                            Commander ces cartes auprès de collectionneurs.<br/><br/>
                         </li>
                         <li>
-                            Enregistrer les plus belles cartes dans vos favoris.
+                            Enregistrer les plus belles cartes dans vos favoris.<br/><br/>
                         </li>
                     </ul>
                 </div>
