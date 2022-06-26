e12d4b29842463b3d74fb26079d2204c500c15fe
loicpaolucci <loic6701@laposte.net>
2022-06-26 17:37:34 +0200

    appi

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -5,35 +5,6 @@ import "assets/styles/profile.scss";
 
 
 const Profile = () => {
-<<<<<<< HEAD
-    const user = useAtomValue(userAtom);
-    console.log(user)
-    
-    return(
-
-        <>
-       
-<div class="container mt-5">
-    
-    <div class="row d-flex justify-content-center">
-        
-        <div class="col-md-7">
-            
-            <div class="card p-3 py-4">
-                
-                <div class="text-center">
-                    <img src="https://i.imgur.com/bDLhJiP.jpg" width="100" class="rounded-circle"/>
-                </div>
-                
-                <div class="text-center mt-3">
-                    <span class="bg-secondary p-1 px-4 rounded text-white">Pro</span>
-                    <h3 class="mt-2 mb-0">  {user} </h3>
-                    <h3> {user.first_name}</h3>
-                    <h4>Email: {user.email} </h4>
-                   
-                </div>
-                
-=======
     const [user, setUser] = useAtom(userAtom);
 
     const editUser = () => {
@@ -72,7 +43,6 @@ const Profile = () => {
                         </form>
                     </div>
                 </div>
->>>>>>> f97a952282ae29c665331b2dcdb10ba88cacca02
             </div>
         </div>
     )
